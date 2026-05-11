e4d6492c25ea88e8a8186d2423d4214fb662b80d
Chugchilan <dylanbaquerizo26@gmail.com>
2026-05-11 17:58:24 -0500

    Configuracion diseño

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ content: [
     "./src/**/*.{js,ts,jsx,tsx,vue}",
 ],
 theme: {
-    extend: {},
+    extend: {
+        colors: {
+            'primario': '#1e40af', // Un azul académico
+            'secundario': '#10b981', // Un verde para tareas completadas
+            'alerta': '#ef4444',   // Rojo para tareas urgentes
+        },
+    },
 },
 plugins: [],
 }
+// tailwind.config.js
